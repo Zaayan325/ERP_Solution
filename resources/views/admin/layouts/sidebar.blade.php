2fb4b440335b47ec6ea0f72c00fc6e0231e41a66
Zaayan325 <150224587+Zaayan325@users.noreply.github.com>
2024-07-19 23:54:48 +0500

    updated

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -16,8 +16,8 @@
     </a>
     <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
     <li>
-        <a href="{{ route('categories.index') }}">
-          <i class="bi bi-circle"></i><span>View Category</span>
+        <a href="{{ route('uoms.index') }}">
+          <i class="bi bi-circle"></i><span>View Units</span>
         </a>
       </li>
     <li>
@@ -40,27 +40,23 @@
 
   <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-      <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
+      <i class="bi bi-journal-text"></i><span>Warehouse</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      
       <li>
-        <a href="forms-elements.html">
-          <i class="bi bi-circle"></i><span>Form Elements</span>
+        <a href="{{ route('warehouses.index') }}">
+          <i class="bi bi-circle"></i><span>View Warehouse</span>
         </a>
       </li>
       <li>
-        <a href="forms-layouts.html">
-          <i class="bi bi-circle"></i><span>Form Layouts</span>
+        <a href="{{ route('warehouses.create') }}">
+          <i class="bi bi-circle"></i><span>Add Warehouse</span>
         </a>
       </li>
       <li>
-        <a href="forms-editors.html">
-          <i class="bi bi-circle"></i><span>Form Editors</span>
-        </a>
-      </li>
-      <li>
-        <a href="forms-validation.html">
-          <i class="bi bi-circle"></i><span>Form Validation</span>
+        <a href="{{ route('warehouse_stock.index') }}">
+          <i class="bi bi-circle"></i><span>View Warehouse Stock</span>
         </a>
       </li>
     </ul>
@@ -68,17 +64,22 @@
 
   <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-      <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+      <i class="bi bi-layout-text-window-reverse"></i><span>Stock</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
       <li>
-        <a href="tables-general.html">
-          <i class="bi bi-circle"></i><span>General Tables</span>
+        <a href="{{ route('stock_brands.index') }}">
+          <i class="bi bi-circle"></i><span>View Stock Brands</span>
         </a>
       </li>
       <li>
-        <a href="tables-data.html">
-          <i class="bi bi-circle"></i><span>Data Tables</span>
+        <a href="{{ route('stock_categories.index') }}">
+          <i class="bi bi-circle"></i><span>View Stock Categories</span>
+        </a>
+      </li>
+      <li>
+        <a href="{{ route('stock_uoms.index') }}">
+          <i class="bi bi-circle"></i><span>View Stock Unit</span>
         </a>
       </li>
     </ul>
@@ -86,47 +87,58 @@
 
   <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-      <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
+      <i class="bi bi-bar-chart"></i><span>Suppliers</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-      <li>
-        <a href="charts-chartjs.html">
-          <i class="bi bi-circle"></i><span>Chart.js</span>
+    <li>
+        <a href="{{ route('suppliers.index') }}">
+          <i class="bi bi-circle"></i><span>View Suppliers</span>
         </a>
       </li>
       <li>
-        <a href="charts-apexcharts.html">
-          <i class="bi bi-circle"></i><span>ApexCharts</span>
+        <a href="{{ route('suppliers.create') }}">
+          <i class="bi bi-circle"></i><span>Add Suppliers</span>
         </a>
       </li>
-      <li>
-        <a href="charts-echarts.html">
-          <i class="bi bi-circle"></i><span>ECharts</span>
-        </a>
-      </li>
+      
     </ul>
   </li><!-- End Charts Nav -->
 
   <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-      <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
+      <i class="bi bi-gem"></i><span>Customers</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
     <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-      <li>
-        <a href="icons-bootstrap.html">
-          <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
+    <li>
+        <a href="{{ route('customers.index') }}">
+          <i class="bi bi-circle"></i><span>View Customers</span>
         </a>
       </li>
       <li>
-        <a href="icons-remix.html">
-          <i class="bi bi-circle"></i><span>Remix Icons</span>
+        <a href="{{ route('customers.index') }}">
+          <i class="bi bi-circle"></i><span>Add Customers</span>
+        </a>
+      </li>
+      
+    </ul>
+  </li><!-- End Icons Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-gem"></i><span>Inventory</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+    <li>
+        <a href="{{ route('inventory.index') }}">
+          <i class="bi bi-circle"></i><span>View Inventory</span>
         </a>
       </li>
       <li>
-        <a href="icons-boxicons.html">
-          <i class="bi bi-circle"></i><span>Boxicons</span>
+        <a href="{{ route('inventory.create') }}">
+          <i class="bi bi-circle"></i><span>Add Inventory</span>
         </a>
       </li>
+      
     </ul>
   </li><!-- End Icons Nav -->
 
