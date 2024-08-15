adf6b757215b5eba285695b28ec3be54448d8144
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-08-15 22:27:13 +0500

    login form integrate

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/l', function () {
+    return view('login');
+});
 
 
 // Route::get('/dashboard', function () {
