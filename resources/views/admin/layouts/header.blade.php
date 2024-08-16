caec2b96dad82edcae1dbb4af88621fc9cf965ab
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-08-16 14:18:12 +0500

    some changes

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -2,8 +2,8 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
+      <a href="{{ route('dashboard') }}" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.jpg" alt="">
         <span class="d-none d-lg-block">Track Point</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -128,7 +128,6 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
           </a><!-- End Profile Iamge Icon -->
 
