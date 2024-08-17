cbcec3355999df7f72878aecb216a3f22fac2db9
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-08-17 15:36:50 +0500

    web update

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/l', function () {
     return view('login');
 });
+
+
 
 
 // Route::get('/dashboard', function () {
