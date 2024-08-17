93a00a044e26af0be83968ed8d316cc82d8a596c
abdullahbinsaba <abdullahbinsaba2209E@aptechgdn.net>
2024-08-17 12:02:33 +0500

    one page aded

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -171,7 +171,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
+              <a class="dropdown-item d-flex align-items-center" href="{{ route('login') }}">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.layouts.main')
+
+@section('content')
+    <div class="pagetitle">
+        <h1>Add Supplier</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                <li class="breadcrumb-item active">error 404</li>
+            </ol>
+        </nav>
+    </div>
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Error Page 404</h5>
+                        <h1>Hi Sorry for disturbance
+                        </h1>
+                        <h3>
+                            You Got to the wrong page
+                        </h3>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
